ea774753049507c95d3e630b84dc749f405c917c
Mohammad Adib <m.a.adib96@gmail.com>
2013-04-14 19:01:47 -0700

    Commit
    Made open source

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Flood_Fill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Panel = new System.Windows.Forms.Panel();
+            this.SmCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.BackColor = System.Drawing.Color.White;
+            this.Panel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Panel.BackgroundImage")));
+            this.Panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Panel.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Panel.Location = new System.Drawing.Point(8, 8);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(232, 232);
+            this.Panel.TabIndex = 3;
+            this.Panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MousePressed);
+            // 
+            // SmCheckBox
+            // 
+            this.SmCheckBox.AutoSize = true;
+            this.SmCheckBox.Checked = true;
+            this.SmCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SmCheckBox.Location = new System.Drawing.Point(80, 248);
+            this.SmCheckBox.Name = "SmCheckBox";
+            this.SmCheckBox.Size = new System.Drawing.Size(84, 17);
+            this.SmCheckBox.TabIndex = 4;
+            this.SmCheckBox.Text = "Slow-Motion";
+            this.SmCheckBox.UseVisualStyleBackColor = true;
+            this.SmCheckBox.CheckedChanged += new System.EventHandler(this.SmCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(250, 270);
+            this.Controls.Add(this.SmCheckBox);
+            this.Controls.Add(this.Panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flood Fill";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.CheckBox SmCheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Flood_Fill
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        int x, y, count;
+        Color[,] pixels = new Color[232,232];
+        bool slowMo; int limit = 200;
+        bool A, B, C, D, TERMINATE;
+
+        public Form1()
+        {
+            InitializeComponent();
+            slowMo = SmCheckBox.Checked;
+            image = (Bitmap)Panel.BackgroundImage;
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    pixels[i,j] = image.GetPixel(i,j);
+                }
+            }
+        }
+
+        private void MousePressed(object sender, MouseEventArgs e)
+        {
+            PixelOn(x=e.X, y=e.Y);
+            // Begin fill by checking in any direction to get into the recursive loop
+            try
+            {
+                CheckDown();
+                Draw();
+            }
+            catch (StackOverflowException)
+            {
+                MessageBox.Show("Uh Oh! Stack Overflow :P");
+            }
+        }
+
+        private void CheckUp()
+        {
+            PixelOn(x, --y);
+            ValidateNextMove();
+            while (!TERMINATE)
+            {
+                if (!C)
+                {
+                    CheckRight();
+                }
+                if (!D)
+                {
+                    CheckLeft();
+                }
+                if (!A)
+                {
+                    CheckUp();
+                }
+                if (!B)
+                {
+                    CheckDown();
+                }
+                ValidateNextMove();
+            }
+            ++y;
+        }
+
+        private void CheckDown()
+        {            
+            PixelOn(x, ++y);
+            ValidateNextMove();
+            while (!TERMINATE)
+            {
+                if (!C)
+                {
+                    CheckRight();
+                }
+                if (!D)
+                {
+                    CheckLeft();
+                }
+                if (!B)
+                {
+                    CheckDown();
+                }
+                if (!A)
+                {
+                    CheckUp();
+                }
+                ValidateNextMove();
+            }
+            --y;
+        }
+
+        private void CheckLeft()
+        {
+            PixelOn(--x, y);
+            ValidateNextMove();
+            while (!TERMINATE)
+            {
+                if (!A)
+                {
+                    CheckUp();
+                }
+                if (!B)
+                {
+                    CheckDown();
+                }                
+                if (!D)
+                {
+                    CheckLeft();
+                }
+                if (!C)
+                {
+                    CheckRight();
+                }
+                ValidateNextMove();
+            }
+            ++x;
+        }
+
+        private void CheckRight()
+        {
+            PixelOn(++x, y);
+            ValidateNextMove();
+            while (!TERMINATE)
+            {
+                if (!B)
+                {
+                    CheckDown();
+                }
+                if (!A)
+                {
+                    CheckUp();
+                }                
+                if (!C)
+                {
+                    CheckRight();
+                }
+                if (!D)
+                {
+                    CheckLeft();
+                }
+                ValidateNextMove();
+            }
+            --x;
+        }
+
+        private void ValidateNextMove()
+        {
+            count++;
+            Color a, b, c, d;
+            a = pixels[x, y - 1];
+            b = pixels[x, y + 1];
+            c = pixels[x + 1, y];
+            d = pixels[x - 1, y];
+            A = (a.R < 200);
+            B = (b.R < 200);
+            C = (c.R < 200);
+            D = (d.R < 200);
+            TERMINATE = (A && B && C && D);            
+            if (count == limit)
+            {
+                Draw();
+                count = 0;
+            }
+        }
+
+        private void PixelOn(int X, int Y)
+        {
+            pixels[X, Y] = Color.Black;            
+        }
+
+        private void Draw()
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    if (pixels[i, j].R == 0)
+                    {
+                        image.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+            Graphics g = Panel.CreateGraphics();
+            g.DrawImage(image, new Point(0,0));
+            g.Dispose();
+        }
+
+        private void SmCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            slowMo = SmCheckBox.Checked;
+            if (slowMo) limit = 200; else limit = 0;
+        }
+    }
+}
